942ec21cd0e91a11c6707f1873cfe0a2f75c8320
uva39 <36david07@gmail.com>
2026-05-10 17:58:58 +0900

    gpt2.py 구현

--- models/gpt2.py
+++ models/gpt2.py
@@ -47,19 +47,16 @@ class GPT2Model(GPTPreTrainedModel):
     input_shape = input_ids.size()
     seq_length = input_shape[1]
 
-    inputs_embeds = None
-
-    ### YOUR CODE HERE
-    raise NotImplementedError
+    inputs_embeds = self.word_embedding(input_ids)
 
 
     pos_ids = self.position_ids[:, :seq_length]
-    pos_embeds = None
+    pos_embeds = self.pos_embedding(pos_ids)
 
-    ### TODO: Use pos_ids to get position embedding from self.pos_embedding into pos_embeds.
-    ###       Then, add two embeddings together; then apply dropout and return.
-    ### YOUR CODE HERE
-    raise NotImplementedError
+    embeds = inputs_embeds + pos_embeds
+    embeds = self.embed_dropout(embeds)
+    return embeds
+
 
 
   def encode(self, hidden_states, attention_mask):
@@ -105,8 +102,7 @@ class GPT2Model(GPTPreTrainedModel):
 
       return hidden_state(s) * E^T
     """
-    ### YOUR CODE HERE
-    raise NotImplementedError
+    return hidden_state @ self.word_embedding.weight.T
 
 
   @classmethod
