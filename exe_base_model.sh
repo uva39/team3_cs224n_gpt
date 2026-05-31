97041b0aec96a4ffc17a3477ab602cc157be450e
jeongbyeolsol <bdjssy548@gmail.com>
2026-05-31 13:26:28 +0000

    expriments

---
+++ exe_base_model.sh
@@ -0,0 +1,57 @@
+﻿#!/usr/bin/env bash
+set -euo pipefail
+
+mkdir -p checkpoints runs
+
+LOG_FILE="log.txt"
+
+exec > "$LOG_FILE"
+
+#echo "//////////////////////////////////////////"
+#echo "START LAST LAYER BASELINE"
+#echo "//////////////////////////////////////////"
+
+#PYTHONPATH=. python -u ./scripts/run_classifier.py \
+#    --use_gpu \
+#    --fine-tune-mode last-linear-layer \
+#    --lr 1e-3 \
+#    --epochs 10 \
+#    --batch_size 8 \
+#    --hidden_dropout_prob 0.2 \
+#    --sst-filepath checkpoints/base-linear-sst.pt \
+#    --cfimdb-filepath checkpoints/base-linear-cfimdb.pt \
+#    --predictions-prefix base-linear-
+
+echo "//////////////////////////////////////////"
+echo "START FULL MODEL BASELINE_1"
+echo "//////////////////////////////////////////"
+
+PYTHONPATH=. python -u ./scripts/run_classifier.py \
+    --use_gpu \
+    --fine-tune-mode full-model \
+    --lr 1e-5 \
+    --epochs 5 \
+    --batch_size 8 \
+    --hidden_dropout_prob 0.2 \
+    --sst-filepath checkpoints/base-full-lr1e-5-ep5-sst.pt \
+    --cfimdb-filepath checkpoints/base-full-lr1e-5-ep5-cfimdb.pt \
+    --predictions-prefix base-full-lr1e-5-ep5-
+
+echo "//////////////////////////////////////////"
+echo "START FULL MODEL BASELINE_2"
+echo "//////////////////////////////////////////"
+
+PYTHONPATH=. python -u ./scripts/run_classifier.py \
+    --use_gpu \
+    --fine-tune-mode full-model \
+    --lr 2e-5 \
+    --epochs 5 \
+    --batch_size 8 \
+    --hidden_dropout_prob 0.2 \
+    --sst-filepath checkpoints/base-full-lr2e-5-ep5-sst.pt \
+    --cfimdb-filepath checkpoints/base-full-lr2e-5-ep5-cfimdb.pt \
+    --predictions-prefix base-full-lr2e-5-ep5-
+
+echo "//////////////////////////////////////////"
+echo "ALL BASELINE EXPERIMENTS DONE"
+echo "//////////////////////////////////////////"
